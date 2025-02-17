e9b0f3f2f5f63529669b309966919a9009568048
Evellyn Lindoso <evellserrao@icloud.com>
2025-02-17 01:59:50 -0300

    estilo finalizado

---
+++ projeto_spotify/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {faCirclePlay} from "@fortawesome/free-solid-svg-icons"
+import ItemList from './ItemList'
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <ItemList />
+
+        
+
+        <ItemList />
+    </div>
+  )
+}
+
+export default Main
