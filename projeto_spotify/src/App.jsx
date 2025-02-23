c256985f28ee5bfd79fccbfaef45317bff0f74fc
Evellyn Lindoso <evellserrao@icloud.com>
2025-02-23 13:30:57 -0300

    aula 3 finalizada

--- projeto_spotify/src/App.jsx
+++ projeto_spotify/src/App.jsx
@@ -1,27 +1,26 @@
-import Header from "./components/Header"
-import Main from "./components/Main"
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Artist from "./pages/Artist"
-import Artists from "./pages/Artists"
-import Home from "./pages/Home"
-import Song from "./pages/Song"
-import Songs from "./pages/Songs"
+import React from "react";
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Artists from "./pages/Artists";
+import Artist from "./pages/Artist";
+import Songs from "./pages/Songs";
+import Song from "./pages/Song";
 
-
-function App() {
-
+const App = () => {
   return (
     <BrowserRouter>
       <Header />
+
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/artist/:id" element={<Artist />}/>
-        <Route path="/artists" element={<Artists />}/>
-        <Route path="/song/:id" element={<Song />}/>
-        <Route path="/songs" element={<Songs />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/artists" element={<Artists />} />
+        <Route path="/artist/:id" element={<Artist />} />
+        <Route path="/songs" element={<Songs />} />
+        <Route path="/song/:id" element={<Song />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- projeto_spotify/src/components/Header.jsx
+++ projeto_spotify/src/components/Header.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import logoSpotify from '../assets/logo/spotify-logo.png'
-import { Link } from 'react-router-dom'
+import React from "react";
+import logoSpotify from "../assets/logo/spotify-logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className='header'>
-        <Link to = "/">
-          <img src= {logoSpotify} alt="Logo do Spotify" />
-        </Link>
+    <div className="header">
+      <Link to="/">
+        <img src={logoSpotify} alt="Logo do Spotify" />
+      </Link>
 
-        <Link className= "header__link" to="">
-             <h1>Spotify</h1>
-        </Link>
+      <Link to="/" className="header__link">
+        <h1>Spotify</h1>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
